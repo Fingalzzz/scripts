6de4b84e1f4f50d5fef292c00e95d2b11811c228
fingalzzz <fingal.zhou@hotmail.com>
2020-08-30 13:43:20 +0800

    yingzhou.sh v1.0

--- yingzhou.sh
+++ yingzhou.sh
@@ -1,23 +1,16 @@
 # confirm pos 400 600
 # finish pos  400 1300
 
-x11=(50 150)
-y11=(1500 1400)
+x11=(50 150 260 600 200 500 800 600 800 950 1050)
+y11=(1500 1400 1500 1400 1800 1700 1500 1800 1700 1750 1680)
 num=${#x11[*]}
-for i in `seq 1 $num`;do
-    input tap ${x11[$i-1]} ${y11[$i-1]}
-    sleep 0.5;
-    input tap 400 600
-    sleep 0.5;
-    input tap 400 600
-done
-let waitSecs=3*60-25-$num-1
-sleep $waitSecs;
-for i in `seq 1 $num`; do
-    input tap ${x11[$i-1]} ${y11[$i-1]}
-    sleep 0.5;
-    input tap 400 1300
-    sleep 0.5;
-    input tap ${x11[$i-1]} ${y11[$i-1]};
-    sleep 1;
+while true; do
+    for i in `seq 1 $num`;do
+        input tap ${x11[$i-1]} ${y11[$i-1]}
+        sleep 0.5;
+        input tap 400 600
+        sleep 0.5;
+        input tap 400 600
+    done
+    sleep 185;
 done
