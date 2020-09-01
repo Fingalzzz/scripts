b3dd3ae2ac436f62b50196b1381e6cd9c18cf1fe
Fingalzzz <fingal.zhou@gmail.com>
2020-09-01 18:35:46 +0800

    Fix some syntx

--- yingtian.sh
+++ yingtian.sh
@@ -6,17 +6,18 @@ y11=(1500 1400 1500 1400 1800 1700 1500 1800 1700 1750 1680)
 num=${#x11[*]}
 while true; do
     for i in `seq 1 $num`;do
-        let x=${x11[$i-1]} + $RANDOM % 20 - 10
-        let y=${y11[$i-1]} + $RANDOM % 20 - 10
+        let x=${x11[$i-1]}+$RANDOM%20-10
+        let y=${y11[$i-1]}+$RANDOM%20-10
         input tap $x $y
-        let r=$RANDOM % 20 / 100
+        let r=$RANDOM%20/100
         sleep 0.5;
-        let x=400 + $RANDOM % 10 - 5
-        let y=600 + $RANDOM % 10 - 5
+        let x=400+$RANDOM%10-5
+        let y=600+$RANDOM%10-5
         input tap $x $y
         sleep 0.4;
         input tap $x $y
         #input tap 400 600
     done
-    sleep 185;
+    sleep 190;
 done
+
