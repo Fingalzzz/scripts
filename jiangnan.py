2adcfbbda3b547af88648f8997fc9321039d2a8a
Fingalzzz <Fingal.ZHOU@gmail.com>
2020-09-15 22:33:33 +0800

    Add a helper function to set a delay time after each tap.

--- jiangnan.py
+++ jiangnan.py
@@ -1,6 +1,8 @@
 from AutoClick import *
 import time
 
+VERSION_CODE = "1.1.1"
+
 WELL_NUM = 36
 WELL_HEAD = (520, 1256)
 EMPTY_WELL_LIST = [5, 7, 10, 14, 21, 22, 25, 27, 30]
@@ -19,11 +21,15 @@ SWIPE_MAP = (908, 1648, 908 - 776, 1648 - 599, 1)
 SWIPE_SUZHOU_WELL = (10, 722, 1075, 1100, 3.5)
 
 
-def suzhou_well():
-    device.click(*FUYING)
-    device.click(*DIJI)
-    device.click(*FUYING)
+def tap(coordinate: (int, int)):
+    device.click(*coordinate)
     time.sleep(0.2)
+
+
+def suzhou_well():
+    tap(FUYING)
+    tap(DIJI)
+    tap(FUYING)
     device.swipe(*SWIPE_SUZHOU_WELL)
     time.sleep(2)
     for i in range(36):
@@ -34,36 +40,30 @@ def suzhou_well():
         well_x = WELL_HEAD[0] + (-quotient + remainder) * X_RANGE
         well_y = WELL_HEAD[1] + (quotient + remainder) * Y_RANGE
         coor_well = (well_x, well_y)
-        device.click(*coor_well)
-        time.sleep(0.1)
+        tap(coor_well)
         device.click(380, 627)
-        time.sleep(0.1)
         device.click(380, 627)
-        time.sleep(0.1)
 
 
 def move_to_suzhou():
-    device.click(*FUYING)
-    device.click(*MAP)
-    time.sleep(0.5)
+    tap(FUYING)
+    tap(MAP)
     device.swipe(*SWIPE_MAP)
-    device.click(*SUZHOU)
-    time.sleep(5)
-    device.click(*ZHIDAOLE)
-    time.sleep(0.3)
+    tap(SUZHOU)
+    time.sleep(7)
+    tap(ZHIDAOLE)
 
 
 def move_to_yingtian():
-    device.click(*FUYING)
-    device.click(*MAP)
-    time.sleep(0.5)
-    device.click(*YINGTIAN)
-    time.sleep(5)
-    device.click(*ZHIDAOLE)
-    time.sleep(0.3)
+    tap(FUYING)
+    tap(MAP)
+    tap(YINGTIAN)
+    time.sleep(7)
+    tap(ZHIDAOLE)
 
 
 if __name__ == "__main__":
+    print(VERSION_CODE)
     while True:
         move_to_yingtian()
         move_to_suzhou()
