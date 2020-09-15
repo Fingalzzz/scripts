6c262b1301cffc905f0428c52c5fd4f2e0b47bf5
Fingalzzz <Fingal.ZHOU@gmail.com>
2020-09-15 08:27:25 +0800

    remove outdated version of jiangnan.py

--- connect.py
+++
@@ -1,9 +0,0 @@
-from ppadb.client import Client as adbClient
-
-
-def adbConnect():
-    client = adbClient()
-    client.remote_connect("192.168.199.202", 5792)
-    device = client.device("192.168.199.202:5792")
-#    device.shell("input tap 500 500")
-    return device
--- yingzhouRun.bat
+++
@@ -1,3 +0,0 @@
-adb connect 192.168.199.202:5792
-adb push ./yingtian.sh /sdcard
-adb shell sh /sdcard/yingtian.sh
